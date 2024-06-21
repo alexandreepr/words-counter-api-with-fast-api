3b7462bdf9e7cee2781b50d845e4fb1cb5fea515
Alexandre Pola <alexandre.polla@gmail.com>
2024-06-21 13:15:51 -0300

    feat: implement WordsCounterResponse

--- backend/app/api/v1/words_counter.py
+++ backend/app/api/v1/words_counter.py
@@ -1,21 +1,32 @@
 from fastapi import APIRouter, HTTPException
 
-from ...core.schemas import WordsCounterRequest
+from ...core.schemas import WordsCounterRequest, WordsCounterResponse
 from ...core.logger import logger
-
 
 router = APIRouter(tags=["words-counter"])
 
-@router.post("/words-counter")
-async def post_words_counter(request: WordsCounterRequest):
+
+@router.post(
+    "/words-counter",
+    response_description="Object containing a key `words_count` with the number of words in the input string.",
+)
+async def post_words_counter(request: WordsCounterRequest) -> WordsCounterResponse:
+    """
+    Count the number of words in the input string.
+
+    - **words_string**: The input string to count the number of words in.
+    """
     words_string = request.words_string.strip()
 
     if not words_string:
         logger.error('POST /')
-        raise HTTPException(status_code=400, detail="Input string is empty or contains only whitespace.")
+        raise HTTPException(
+            status_code=422,
+            detail="Input string is empty or contains only whitespace."
+        )
 
     words_count = len(words_string.split())
 
     logger.info('POST /')
 
-    return { "words_count": words_count }
+    return {"words_count": words_count}
--- backend/app/core/schemas.py
+++ backend/app/core/schemas.py
@@ -3,3 +3,5 @@ from pydantic import BaseModel
 class WordsCounterRequest(BaseModel):
     words_string: str
 
+class WordsCounterResponse(BaseModel):
+    words_count: int
