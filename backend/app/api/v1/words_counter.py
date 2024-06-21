b9278734c536177c7653837088b4f4fd8901b796
Alexandre Pola <alexandre.polla@gmail.com>
2024-06-21 10:28:35 -0300

    feat: implement logger on backend

--- backend/app/api/v1/words_counter.py
+++ backend/app/api/v1/words_counter.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter
-from http.client import HTTPException
+from fastapi import APIRouter, HTTPException
 
 from ...core.schemas import WordsCounterRequest
+from ...core.logger import logger
 
 
 router = APIRouter(tags=["words-counter"])
@@ -11,8 +11,11 @@ async def post_words_counter(request: WordsCounterRequest):
     words_string = request.words_string.strip()
 
     if not words_string:
+        logger.error('POST /')
         raise HTTPException(status_code=400, detail="Input string is empty or contains only whitespace.")
 
     words_count = len(words_string.split())
+
+    logger.info('POST /')
 
     return { "words_count": words_count }
--- backend/app/main.py
+++ backend/app/main.py
@@ -2,11 +2,14 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from .core.config import settings
+from .core.logger import logger
 from .api import router as api_router
 
 app = FastAPI()
 
 origins = settings.get("ALLOWED_ORIGINS")
+
+logger.info('API is starting up')
 
 app.add_middleware(
     CORSMiddleware,
